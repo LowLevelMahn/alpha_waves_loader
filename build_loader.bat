f34e0c67995f60190d9dd192956732d7850fab0a
LowLevelMahn <dl.soluz@gmx.net>
2022-02-20 07:40:40 +0100

    added stripped loader

---
+++ build_loader.bat
@@ -0,0 +1,19 @@
+@echo off
+
+set my_root=F:\projects\fun\dos_games_rev
+set tools_dir=%my_root%\tools
+set org_dir=%my_root%\alpha_waves_dev\tests\alpha
+
+set uasm_exe=%tools_dir%\uasm_x64\uasm64.exe
+
+set WATCOM=%tools_dir%\open-watcom-2_0-c-win-x64
+set WATCOM_BIN=%watcom%\binnt64
+set INCLUDE=%watcom%\h
+set PATH=%WATCOM_BIN%;%PATH%
+set wlink_exe=%watcom_bin%\wlink.exe
+
+%uasm_exe% loader.asm 
+%wlink_exe% name loader.com format dos com file loader.obj
+copy loader.com %org_dir%
+
+pause
